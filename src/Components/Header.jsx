78486c8fe1de86f8e4833b9a8a35798194bdd97b
ghostofphantom <itsmehassxn@gmail.com>
2025-08-30 18:23:30 +0500

    header.created

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import  '../App.css'; 
+import logo from '../assets/logo.png';
 
-function Header() {
-  return (
-    <button className='btn btn-danger'>
-        Button
-    </button>
+  
+  function Header() {
+    return (
+    <header>
+      
+      <div className="top-notification">
+        <p>7-DAYS HASSLE FREE RETURN AND EXCHANGE POLICY!</p>
+      </div>
+
+      <div className="header-main">
+        <div className="logo">
+          <img src={logo} alt="Kinari" />
+        </div>
+        <nav>
+          <ul className="nav-links">
+            <li><a href="#">NEW ARRIVALS</a></li>
+            <li><a href="#">SALE</a></li>
+            <li><a href="#">PURE CHARMEUSE SILK</a></li>
+            <li><a href="#">CHIFFON COLLECTION</a></li>
+          </ul>
+        </nav>
+
+        <div className="header-right">
+          <ul className="user-actions">
+            <li><a href="#">ACCOUNT</a></li>
+            <li><a href="#">SEARCH</a></li>
+            <li><a href="#">CART (0) </a></li>
+          </ul>
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default Header
+  export default Header
+   
