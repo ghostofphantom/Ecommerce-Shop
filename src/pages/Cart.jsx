67018d37a6f423d516ebd548b80500ababbb2cb8
ghostofphantom <itsmehassxn@gmail.com>
2025-09-08 17:06:16 +0500

    Cartupdated

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import img from '../assets/Cart.png'
 
 
+
 function Cart() {
   return (
    <div className="container">
     <div className="row">
-      <div className="col-md-6">
+      <div className="col-md-8">
         <div className="checkout">
           <img className='cart.img' src={img} alt="" />
         </div>
       </div>
-      <div className="col-md-6">
+      <div className="col-md-4">
         <div className="description">
           <p>Midnight Petals Linen 2pc</p>
           <p>Rs.3,495.00</p>
+          <button className='Checkout'> ADD TO CART </button>
+          <button className='Buynow'> BUY IT NOW </button>
         </div>
       </div>
     </div>
