4fe07b8fc19471098cb3faf73b242a21cc2bbd67
Badarp2 <waqas.badar9tv@gmail.com>
2025-09-11 17:01:50 +0500

    CartcontextUpdated

--- src/context/createContext.jsx
+++ src/context/createContext.jsx
@@ -1,12 +1,23 @@
 import React, { createContext } from 'react'
-
+import pic from '../assets/Cart.png'
 
  export  const cartContext = createContext()
 
 
-function CreateContext(children) {
+function CreateContext({children}) {
 
-        const product = "This prod one"
+        const product = <div> <img src={pic} alt="" />
+        
+             <p>Midnight Petals Linen 2pc</p>
+          <p>Rs.3,495.00</p>
+          <button className='Checkout'> ADD TO CART </button>
+          <button className='Buynow'> BUY IT NOW </button>
+          <p className='lowerContent'>Wear the Art You Feel Premium Linen, Unstitched for You.🎨</p>
+        
+        
+        
+        
+         </div>
 
 
   return (
