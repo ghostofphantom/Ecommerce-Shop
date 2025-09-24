1534352a3000f2dcf1eca124d164d6d3e6120972
ghostofphantom <itsmehassxn@gmail.com>
2025-09-24 17:17:22 +0500

    Checkoutform

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -13,7 +13,26 @@ function Checkout() {
         <div className="col-md-6">
           <div className="paynow">
             <h2>Contact</h2>
-            <input type="email" className="form-control" placeholder="Email or mobile phone number" />
+           <div className="formBox">
+             <input type="email" className="form-control" placeholder="Email or mobile phone number" />
+           </div>
+           <div className="formBox">
+             <input type="checkbox" className='me-2 mt-3'/>
+             <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
+           </div>
+           <div className="formBox mt-3">
+             <select className='form-control'>
+              <option value="Pakistan">Pakistan</option>
+              <option value="Pakistan">Pakistan2</option>
+              <option value="Pakistan">Pakistan3</option>
+              <option value="Pakistan">Pakistan4</option>
+             </select>
+           </div>
+           <div className="formBox d-flex mt-3">
+             <input type="text" className='form-control me-4' placeholder='First Name'  />
+             <input type="text" className='form-control ' placeholder='Last Name'  />
+           </div>
+           
           </div>
         </div>
         <div className="col-md-6">
